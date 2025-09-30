7d21a28d7a757b8f2402d305fecf51472c370359
FreddieAlvarez <alvarez.freddie@ic.edu>
2025-09-29 21:47:03 -0500

    Gifs display on button click

--- js/script.js
+++ js/script.js
@@ -2,28 +2,25 @@ console.log("script.js loaded");
 
 var endpoint = "https://api.giphy.com/v1/gifs/search?api_key=r3aYm2692lk5yNMZNQfztmQ2Tcit58Yr&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
-function getCatGifs() {
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
+const gifContainer = document.querySelector("#gif-container");
+const fetchGifBtn = document.querySelector("#fetch-gif-btn");
+
+fetchGifBtn.addEventListener("click", async () => {
+        try{
+            const response = await fetch(endpoint);
+            const data = await response.json();
             const gifs = data.data;
+
             const output = gifs.map(gif => gif.images.original.url);
 
-            console.log(output);
-
-            const gifContainer = document.getElementById("gifContainer");
             gifContainer.innerHTML = '';
 
             output.forEach(url => {
-                const img = document.createElement('img');
-                img.src = url;
-                gifContainer.appendChild(img);
+                gifContainer.innerHTML += `<img src = "${url}" class = "col-3 mb-3">`;
             });
-        }) 
-    .catch(error => {
-        console.error("Error fetching gifs:", error);
+
+            console.log("GIFs Loaded:", gifs.map(g=> g.images.original.url));
+        } catch (error) {
+            console.error("Error fetching gifs:", error);   
+        }
     });
-}
-
-document.getElementById("searchBtn").addEventListener("click", getCatGifs);
-
