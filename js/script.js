869819c8845ad51b7d68dd478b919b1d6b8e9022
FreddieAlvarez <alvarez.freddie@ic.edu>
2025-09-29 22:06:24 -0500

    dynamic search

--- js/script.js
+++ js/script.js
@@ -4,23 +4,27 @@ var endpoint = "https://api.giphy.com/v1/gifs/search?api_key=r3aYm2692lk5yNMZNQf
 
 const gifContainer = document.querySelector("#gif-container");
 const fetchGifBtn = document.querySelector("#fetch-gif-btn");
+const searchInput = document.querySelector("#search-input");
 
 fetchGifBtn.addEventListener("click", async () => {
-        try{
-            const response = await fetch(endpoint);
-            const data = await response.json();
-            const gifs = data.data;
+    const searchTerm = searchInput.value.trim() || "cats";
+    const dynamicEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=r3aYm2692lk5yNMZNQfztmQ2Tcit58Yr&q=${encodeURIComponent(searchTerm)}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    
+    try{
+        const response = await fetch(dynamicEndpoint);
+        const data = await response.json();
+        const gifs = data.data;
 
-            const output = gifs.map(gif => gif.images.original.url);
+        const output = gifs.map(gif => gif.images.original.url);
 
-            gifContainer.innerHTML = '';
+        gifContainer.innerHTML = '';
 
-            output.forEach(url => {
-                gifContainer.innerHTML += `<img src = "${url}" class = "col-3 mb-3">`;
-            });
+        output.forEach(url => {
+            gifContainer.innerHTML += `<img src = "${url}" class = "col-3 mb-3">`;
+        });
 
-            console.log("GIFs Loaded:", gifs.map(g=> g.images.original.url));
-        } catch (error) {
-            console.error("Error fetching gifs:", error);   
-        }
-    });
+        console.log("GIFs Loaded:", gifs.map(g=> g.images.original.url));
+    } catch (error) {
+        console.error("Error fetching gifs:", error);   
+    }
+});
