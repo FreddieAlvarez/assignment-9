15caa95821b8bb7a230fe56a30a938f7bf53ed24
FreddieAlvarez <alvarez.freddie@ic.edu>
2025-09-29 21:24:00 -0500

    Fetch for cat gifs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("script.js loaded");
+
+var endpoint = "https://api.giphy.com/v1/gifs/search?api_key=r3aYm2692lk5yNMZNQfztmQ2Tcit58Yr&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+function getCatGifs() {
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            const gifs = data.data;
+            const output = gifs.map(gif => gif.images.original.url);
+
+            console.log(output);
+
+            const gifContainer = document.getElementById("gifContainer");
+            gifContainer.innerHTML = '';
+
+            output.forEach(url => {
+                const img = document.createElement('img');
+                img.src = url;
+                gifContainer.appendChild(img);
+            });
+        }) 
+    .catch(error => {
+        console.error("Error fetching gifs:", error);
+    });
+}
+
+document.getElementById("searchBtn").addEventListener("click", getCatGifs);
+
